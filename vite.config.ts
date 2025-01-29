17fb96b910274550e3d3319c158e446523ee24c0
ljq-96 <2433272864@qq.com>
2025-01-29 21:04:02 +0800

    🎡 ci(other): 文件路由

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), Pages()]
 })
