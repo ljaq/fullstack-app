51456dcac71bb26356719830a2aa5f7e28d9a851
ljq-96 <2433272864@qq.com>
2025-02-09 17:24:14 +0800

    ✨ feat: 添加编辑器

--- client/pages/about/index.tsx
+++ client/pages/about/index.tsx
@@ -1,10 +1,15 @@
 import { HomeOutlined } from '@ant-design/icons'
+import { Editor } from '@ljaq/editor'
 
 export default function About() {
-  return <div>about</div>
+  return (
+    <div>
+      <Editor />
+    </div>
+  )
 }
 
 export const pageConfig = {
   name: 'about',
-  icon: <HomeOutlined />
+  icon: <HomeOutlined />,
 }
