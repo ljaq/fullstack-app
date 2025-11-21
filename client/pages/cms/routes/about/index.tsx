8924030daed52ea670eabc490ae356eeec065a76
ljaq <2433272864@qq.com>
2025-11-21 15:59:17 +0800

    ✨ feat: cursorLight

--- client/pages/cms/routes/about/index.tsx
+++ client/pages/cms/routes/about/index.tsx
@@ -1,34 +1,9 @@
 import { HomeOutlined } from '@ant-design/icons'
 import { Editor } from '@ljaq/editor'
-import { Col, Row } from 'antd'
-import { CursorLightContainer, CursorLightItem } from 'client/components/CursorLight'
 
 export default function About() {
   return (
     <div>
-      <CursorLightContainer color='255,0,0'>
-        <Row>
-          <Col span={8}>
-            <CursorLightItem>
-              <div>123</div>
-              <div>123</div>
-            </CursorLightItem>
-          </Col>
-          <Col span={8}>
-            <CursorLightItem>123</CursorLightItem>
-          </Col>
-
-          <Col span={8}>
-            <CursorLightItem>123</CursorLightItem>
-          </Col>
-          <Col span={8}>
-            <CursorLightItem>123</CursorLightItem>
-          </Col>
-          <Col span={8}>
-            <CursorLightItem>123</CursorLightItem>
-          </Col>
-        </Row>
-      </CursorLightContainer>
       <Editor mode='card' />
     </div>
   )
