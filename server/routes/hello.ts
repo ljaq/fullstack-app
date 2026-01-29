40a89fa2532eb730e155a170a59180a2775a5687
ljaq <2433272864@qq.com>
2026-01-29 18:27:17 +0800

    ✨ feat: zob

--- server/routes/hello.ts
+++ server/routes/hello.ts
@@ -1,4 +1,6 @@
 import { Hono } from 'hono'
+import * as z from 'zod'
+import { zValidator } from '@hono/zod-validator'
 
 const helloRoute = new Hono()
   .get('/', c => {
@@ -6,6 +8,23 @@ const helloRoute = new Hono()
   })
   .post('/', c => {
     return c.json({ result: 'Hello World!' })
+  })
+  .get(
+    '/user',
+    zValidator(
+      'query',
+      z.object({
+        name: z.string().min(1),
+      }),
+    ),
+    c => {
+      const { name } = c.req.valid('query')
+      return c.json({ result: `Hello ${name}!` })
+    },
+  )
+  .put('/user/:id', zValidator('json', z.object({ id2: z.string().min(1) })), c => {
+    const { id2 } = c.req.valid('json')
+    return c.json({ result: `Hello ${id2}!` })
   })
 
 export default helloRoute
